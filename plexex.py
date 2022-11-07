4bcb8f17a6b0ef053da72334fbd0288fb6c267cb
Bradley Frank <bradfrank@fastmail.com>
2022-11-06 21:12:52 -0500

    Add csv support.

--- plexex.py
+++ plexex.py
@@ -11,12 +11,12 @@ from configparser import ConfigParser
 import inquirer
 from plexapi.server import PlexServer
 
-TRACK_METADATA = {}
+TRACK_METADATA = []
 
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument("-a", "--album", help="album title", type=str)
 arg_parser.add_argument("-l", "--library", help="plex library", type=str)
-arg_perser.add_argument("-c", "--csv", help="output in csv format", action='store_true')
+arg_parser.add_argument("-c", "--csv", help="output in csv format", action="store_true")
 args = arg_parser.parse_args()
 
 cfg_parser = ConfigParser()
@@ -38,25 +38,35 @@ plex = PlexServer(baseurl, token)
 music = plex.library.section(library)
 results = music.searchAlbums(title=args.album)
 
-if len(results) == 1:
-    album_id = results[0]
-elif len(results) > 1:
+if not results:
+    print("No results found.")
+    sys.exit(1)
+
+if len(results) > 1:
     choices = [album.title for album in results]
-    questions = [inquirer.List("album", "Export which album?", choices)]
+    questions = [inquirer.List("album", "Display which album?", choices)]
     answers = inquirer.prompt(questions)
     album_id = next(album for album in results if album.title == answers["album"])
 else:
-    print("No results found.")
-    sys.exit(0)
+    album_id = results[0]
 
 for track in album_id.tracks():
-    TRACK_METADATA = {
-        "number": track.trackNumber or None,
-        "title": track.title or None,
-        "rating": track.userRating or None,
-        "moods": [mood.tag for mood in track.moods]
-    }
-    # print(f"Track: {track.trackNumber:03} - {track.title}")
-    # print(f"Rating: {int((track.userRating or 0) / 2)}")
-    # print(f"Moods: {', '.join([mood.tag for mood in track.moods])}")
-    # print("")
+    TRACK_METADATA.append(
+        {
+            "number": track.trackNumber or -1,
+            "title": track.title or "",
+            "rating": track.userRating or 0,
+            "moods": [mood.tag for mood in track.moods] or ["None"],
+        }
+    )
+
+if args.csv:
+    print("Track No., Track Title, Rating, Moods")
+    for track in TRACK_METADATA:
+        print(f"{track['number']},{track['title']},{track['rating']},{'; '.join(track['moods'])}")
+else:
+    for track in TRACK_METADATA:
+        print(f"Track: {track['number']} - {track['title']}")
+        print(f"Rating: {track['rating']}")
+        print(f"Moods: {', '.join(track['moods'])}")
+        print("")
